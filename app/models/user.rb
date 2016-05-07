bbc9849c0cfbfb89b8cc06e11793ec4597a57c26
surupaTW <surupa@tutorsweb.com>
2016-05-07 15:38:36 +0530

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  validates_presence_of :email
+
+  def self.new_with_session(params,session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"],without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def self.from_omniauth(auth, current_user)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end
